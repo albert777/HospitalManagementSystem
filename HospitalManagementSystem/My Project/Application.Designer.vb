0a9b44e678880025f4b033b39690b4a5237226bd
Limited7247 <lt-dat@outlook.com>
2016-01-08 14:40:51 +0700

    ljgdjflgjd

--- HospitalManagementSystem/My Project/Application.Designer.vb
+++ HospitalManagementSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.HospitalManagementSystem.frmCategoryManager
+            Me.MainForm = Global.HospitalManagementSystem.frmMain
         End Sub
     End Class
 End Namespace
